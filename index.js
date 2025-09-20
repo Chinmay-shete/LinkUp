5655f3b085c0a177a9219e3b2c2eacd0109aebe2
chinmay <chinmayshete4@gmail.com>
2025-09-21 01:03:23 +0530

    setting up message

--- index.js
+++ index.js
@@ -4,13 +4,35 @@ const indexRouter = require("./routes/index");
 const path = require("path");
 
 const http = require("http");
-const socketIo = require("socket.io"); 
+const socketIo = require("socket.io");
 const srever = http.createServer(app);
 const io = socketIo(srever);
-io.on("connection",function(socket){
-    console.log("new connection")
-})
+let waitingusers = [];
+let rooms = {};
+io.on("connection", function (socket) {
+  socket.on("joinroom", function () {
+    if (waitingusers.length > 0) {
+      let patner = waitingusers.shift();
+      const roomname = `${socket.id}-${patner.id}`;
+      socket.join(roomname);
+      patner.join(roomname);
 
+      io.to(roomname).emit("joined",roomname);
+    } else {
+      waitingusers.push(socket);
+    }
+  });
+  socket.on("message",function(data){
+    socket.broadcast.to(data.room).emit("message",data.message)
+     
+  })
+  socket.on("disconnect", function () {
+    let index = waitingusers.findIndex(
+      (waitingUsers) => waitingUsers.id === socket.id
+    );
+    waitingusers.splice(index, 1);
+  });
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -19,4 +41,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.listen("3000");
+srever.listen("3000");
